54f6347b3b7bf355a75109e83b32515c7c6241db
Michael Krumland-Dunning <axolotlman2@gmail.com>
2023-08-13 21:45:55 -0700

    materials, metals, fuzz, ch. 10

--- camera.h
+++ camera.h
@@ -5,6 +5,8 @@
 
 #include "color.h"
 #include "hittable.h"
+#include "material.h"
+
 
 #include <iostream>
 
@@ -96,12 +98,11 @@ class camera {
             return color(0,0,0);
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            //Simple Diffuse Reflection
-            //vec3 direction = random_on_hemisphere(rec.normal);
-
-            //True Lambertian Reflection
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth-1, world);
+            return color(0,0,0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());

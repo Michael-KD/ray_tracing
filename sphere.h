420dfd5046fe4b870a03506820e049631621b895
Michael Krumland-Dunning <axolotlman2@gmail.com>
2023-08-15 16:36:54 -0700

    bounding boxes and bounding volume heirarchy, 2.3

--- sphere.h
+++ sphere.h
@@ -8,12 +8,22 @@ class sphere : public hittable {
   public:
     // Stationary Sphere
     sphere(point3 _center, double _radius, shared_ptr<material> _material)
-      : center1(_center), radius(_radius), mat(_material), is_moving(false) {}
+      : center1(_center), radius(_radius), mat(_material), is_moving(false)
+    {
+        auto rvec = vec3(radius, radius, radius);
+        bbox = aabb(center1 - rvec, center1 + rvec);
+    }
+
 
     // Moving Sphere
     sphere(point3 _center1, point3 _center2, double _radius, shared_ptr<material> _material)
       : center1(_center1), radius(_radius), mat(_material), is_moving(true)
     {
+        auto rvec = vec3(radius, radius, radius);
+        aabb box1(_center1 - rvec, _center1 + rvec);
+        aabb box2(_center2 - rvec, _center2 + rvec);
+        bbox = aabb(box1, box2);
+
         center_vec = _center2 - _center1;
     }
 
@@ -44,6 +54,8 @@ class sphere : public hittable {
 
         return true;
     }
+    aabb bounding_box() const override { return bbox; }
+
 
   private:
     point3 center1;
@@ -51,6 +63,7 @@ class sphere : public hittable {
     shared_ptr<material> mat;
     bool is_moving;
     vec3 center_vec;
+    aabb bbox;
 
     point3 sphere_center(double time) const {
         // Linearly interpolate from center1 to center2 according to time, where t=0 yields
